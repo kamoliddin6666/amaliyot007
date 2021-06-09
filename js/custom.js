c77be082f45a24b962aa46886174f6da92cfc805
kamoliddin6666 <85392615+kamoliddin6666@users.noreply.github.com>
2021-06-09 14:48:04 +0500

    amalyot007

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay: false,
+        autoplaySpeed: 0,
+    })
+});
+//ftftryguhiuh
+//jhukhujygfyujyg
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true ,
+        dots: false,
+        autoplay: 1000,
+        autoplaySpeed: 2000,
+    })
+});
